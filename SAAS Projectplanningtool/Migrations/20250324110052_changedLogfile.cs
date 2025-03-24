46284400a03fffa4ad9dc2f948513d80a433349d
Patrick Sterneder <patrick.sterneder@gmail.com>
2025-03-24 12:01:11 +0100

    added migration

---
+++ SAAS Projectplanningtool/Migrations/20250324110052_changedLogfile.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SAAS_Projectplanningtool.Migrations
+{
+    /// <inheritdoc />
+    public partial class changedLogfile : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CustomMessage",
+                table: "Logfile",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CustomMessage",
+                table: "Logfile");
+        }
+    }
+}
