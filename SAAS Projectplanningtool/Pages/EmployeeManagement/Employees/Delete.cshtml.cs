77909be14f95f73a9f9dcab90921c06958ec960c
Patrick Sterneder <patrick.sterneder@gmail.com>
2025-04-14 22:55:19 +0200

    EmployeeManagement ( nicht fertig )

---
+++ SAAS Projectplanningtool/Pages/EmployeeManagement/Employees/Delete.cshtml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SAAS_Projectplanningtool.Data;
+using SAAS_Projectplanningtool.Models;
+
+namespace SAAS_Projectplanningtool.Pages.EmployeeManagement.Employees
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly ApplicationDbContext _context;
+
+        public DeleteModel(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Employee Employee { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var employee = await _context.Employee.FirstOrDefaultAsync(m => m.EmployeeId == id);
+
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                Employee = employee;
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var employee = await _context.Employee.FindAsync(id);
+            if (employee != null)
+            {
+                Employee = employee;
+                _context.Employee.Remove(Employee);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ SAAS Projectplanningtool/Pages/EmployeeManagement/Employees/Details.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SAAS_Projectplanningtool.Data;
+using SAAS_Projectplanningtool.Models;
+
+namespace SAAS_Projectplanningtool.Pages.EmployeeManagement.Employees
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly ApplicationDbContext _context;
+
+        public DetailsModel(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public Employee Employee { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var employee = await _context.Employee.FirstOrDefaultAsync(m => m.EmployeeId == id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                Employee = employee;
+            }
+            return Page();
+        }
+    }
+}
