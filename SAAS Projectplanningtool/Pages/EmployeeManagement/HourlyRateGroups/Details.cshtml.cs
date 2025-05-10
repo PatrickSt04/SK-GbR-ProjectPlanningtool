43196faf04eb8b15e00db092f4c16f1aab7c8fbf
Patrick Sterneder <patrick.sterneder@gmail.com>
2025-05-10 11:15:39 +0200

    HourlyRateGroups Delete or DeleteFlag logic Employees DeleteFlag

--- SAAS Projectplanningtool/Pages/EmployeeManagement/HourlyRateGroups/Details.cshtml.cs
+++ SAAS Projectplanningtool/Pages/EmployeeManagement/HourlyRateGroups/Details.cshtml.cs
@@ -26,6 +26,7 @@ namespace SAAS_Projectplanningtool.Pages.EmployeeManagement.HourlyRateGroups
         }
 
         public HourlyRateGroup HourlyRateGroup { get; set; } = default!;
+        public bool isUsedInEmployee { get; set; } = false;
 
         public async Task<IActionResult> OnGetAsync(string id)
         {
@@ -37,7 +38,11 @@ namespace SAAS_Projectplanningtool.Pages.EmployeeManagement.HourlyRateGroups
                     return NotFound();
                 }
 
-                var hourlyrategroup = await _context.HourlyRateGroup.FirstOrDefaultAsync(m => m.HourlyRateGroupId == id);
+                var hourlyrategroup = await _context.HourlyRateGroup
+                    .Include(e => e.LatestModifier)
+                    .Include(e => e.CreatedByEmployee)
+                    .Include(e => e.Company)
+                    .FirstOrDefaultAsync(m => m.HourlyRateGroupId == id);
                 if (hourlyrategroup == null)
                 {
                     return NotFound();
@@ -46,8 +51,108 @@ namespace SAAS_Projectplanningtool.Pages.EmployeeManagement.HourlyRateGroups
                 {
                     HourlyRateGroup = hourlyrategroup;
                 }
+
+                var executingEmployee = await new CustomUserManager(_context, _userManager).GetEmployeeAsync(_userManager.GetUserId(User));
+                if (executingEmployee == null)
+                {
+                    return NotFound();
+                }
+                var employeesUsingHourlyRateGroup = await _context.Employee
+                    .Include(e => e.HourlyRateGroup)
+                    .Where(e => e.HourlyRateGroupId == id && e.CompanyId == executingEmployee.CompanyId)
+                    .ToListAsync();
+                isUsedInEmployee = employeesUsingHourlyRateGroup.Count > 0;
+
                 await _logger.Log(null, User, null, "/EmployeeManagement/HourlyRateGroups/Details<OnGetAsync>End");
                 return Page();
+            }
+            catch (Exception ex)
+            {
+                return RedirectToPage("/Error", new { id = await _logger.Log(ex, User, null, null) });
+            }
+        }
+        public async Task<IActionResult> OnPostSetDeleteFlagAsync(string? id)
+        {
+            try
+            {
+                await _logger.Log(null, User, null, "/EmployeeManagement/HourlyRateGroups/Details<OnSetDeleteFlag>Begin");
+                if (id == null)
+                {
+                    return NotFound();
+                }
+
+                var hourlyRateGroup = await _context.HourlyRateGroup.FirstOrDefaultAsync(e => e.HourlyRateGroupId == id);
+                if (hourlyRateGroup == null)
+                {
+                    return NotFound();
+                }
+
+                hourlyRateGroup.DeleteFlag = true;
+                hourlyRateGroup = await new CustomObjectModifier(_context, _userManager).AddLatestModificationAsync(User, "Löschkennzeichen gesetzt", hourlyRateGroup, false);
+                _context.HourlyRateGroup.Update(hourlyRateGroup);
+                await _context.SaveChangesAsync();
+
+                await _logger.Log(null, User, null, "/EmployeeManagement/HourlyRateGroups/Details<OnSetDeleteFlag>End");
+                return RedirectToPage("/EmployeeManagement/Index");
+            }
+            catch (Exception ex)
+            {
+                return RedirectToPage("/Error", new { id = await _logger.Log(ex, User, null, null) });
+            }
+        }
+
+        public async Task<IActionResult> OnPostUndoDeleteFlagAsync(string? id)
+        {
+            try
+            {
+                await _logger.Log(null, User, null, "/EmployeeManagement/HourlyRateGroups/Details<OnUndoDeleteFlag>Begin");
+                if (id == null)
+                {
+                    return NotFound();
+                }
+
+                var hourlyRateGroup = await _context.HourlyRateGroup.FirstOrDefaultAsync(e => e.HourlyRateGroupId == id);
+                if (hourlyRateGroup == null)
+                {
+                    return NotFound();
+                }
+
+                hourlyRateGroup.DeleteFlag = false;
+                hourlyRateGroup = await new CustomObjectModifier(_context, _userManager).AddLatestModificationAsync(User, "Löschkennzeichen gesetzt", hourlyRateGroup, false);
+
+                _context.HourlyRateGroup.Update(hourlyRateGroup);
+                await _context.SaveChangesAsync();
+
+                await _logger.Log(null, User, null, "/EmployeeManagement/HourlyRateGroups/Details<OnUndoDeleteFlag>End");
+                return RedirectToPage("/EmployeeManagement/Index");
+            }
+            catch (Exception ex)
+            {
+                return RedirectToPage("/Error", new { id = await _logger.Log(ex, User, null, null) });
+            }
+        }
+
+        public async Task<IActionResult> OnPostDeleteAsync(string? id)
+        {
+            try
+            {
+                await _logger.Log(null, User, null, "/EmployeeManagement/HourlyRateGroups/Details<OnPostDeleteAsync>Begin");
+                if (id == null)
+                {
+                    return NotFound();
+                }
+
+                var hourlyRateGroup = await _context.HourlyRateGroup.FirstOrDefaultAsync(e => e.HourlyRateGroupId == id);
+                if (hourlyRateGroup == null)
+                {
+                    return NotFound();
+                }
+
+                _context.HourlyRateGroup.Remove(hourlyRateGroup);
+                await _context.SaveChangesAsync();
+
+                await _logger.Log(null, User, null, "/EmployeeManagement/HourlyRateGroups/Details<OnPostDeleteAsync>End");
+                return RedirectToPage("/EmployeeManagement/Index");
             }
             catch (Exception ex)
             {
