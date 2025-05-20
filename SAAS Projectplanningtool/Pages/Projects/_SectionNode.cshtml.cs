cd575a0cef57bb73d86038bedb0d4e47d011ccfd
Simon Sterneder <simon.sterneder@gmail.com>
2025-05-20 23:23:20 +0200

    project schedule chtml überarbeiten

---
+++ SAAS Projectplanningtool/Pages/Projects/_SectionNode.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SAAS_Projectplanningtool.Pages.Projects
+{
+    public class _SectionNodeModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
